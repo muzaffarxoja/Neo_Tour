1f68db98bbf5b8930f6a25d5aa83d79e299dc624
muzaffarxoja <muzsamgio@gmail.com>
2024-06-19 20:16:22 +0500

    go router

--- lib/ui/widgets/booking_modal_sheet.dart
+++ lib/ui/widgets/booking_modal_sheet.dart
@@ -5,96 +5,133 @@ import 'package:provider/provider.dart';
 
 import '../../provider/counter_and_number.dart';
 
-
 class BookingModalSheet extends StatelessWidget {
   const BookingModalSheet({Key? key}) : super(key: key);
-
-
 
   // This function is triggered when the floating buttion is pressed
   void show_booking_modal_sheet(BuildContext contex) {
     showModalBottomSheet(
-        isScrollControlled: true,
-        elevation: 5,
-        context: contex,
-        builder: (ctx) => Padding(
-              padding: EdgeInsets.only(
-                  top: 15,
-                  left: 15,
-                  right: 15,
-                  bottom: MediaQuery.of(ctx).viewInsets.bottom + 15),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Info',
-                    style: TextStyle(
-                        color: Color(0XFF141414),
-                        fontSize: 24,
-                        //fontFamily: 'SF Pro Display',
-                        fontWeight: FontWeight.w700),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    'To submit an application for a tour reservation, you need to fill in your information and select the number of people for the reservation',
-                    style: TextStyle(
-                        color: Color(0XFF141414),
-                        fontSize: 14,
-                        //fontFamily: 'SF Pro Display',
-                        fontWeight: FontWeight.w400),
-                  ),
-                  const SizedBox(height: 12),
-                  Text(
-                    'Phone number',
-                    style: TextStyle(
-                        color: Color(0XFF888888),
-                        fontSize: 14,
-                        //fontFamily: 'SF Pro Display',
-                        fontWeight: FontWeight.w400),
-                  ),
-                  const SizedBox(height: 3),
-                  PhoneNumberInputWidget(),
-                  const TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(labelText: 'Name'),
-                  ),
-                  const TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: 'Age'),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-
+      isScrollControlled: true,
+      elevation: 5,
+      context: contex,
+      builder: (ctx) => Padding(
+        padding: EdgeInsets.only(
+            top: 15,
+            left: 15,
+            right: 15,
+            bottom: MediaQuery.of(ctx).viewInsets.bottom + 15),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Info',
+              style: TextStyle(
+                  color: Color(0XFF141414),
+                  fontSize: 24,
+                  //fontFamily: 'SF Pro Display',
+                  fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'To submit an application for a tour reservation, you need to fill in your information and select the number of people for the reservation',
+              style: TextStyle(
+                  color: Color(0XFF141414),
+                  fontSize: 14,
+                  //fontFamily: 'SF Pro Display',
+                  fontWeight: FontWeight.w400),
+            ),
+            const SizedBox(height: 12),
+            Text(
+              'Phone number',
+              style: TextStyle(
+                  color: Color(0XFF888888),
+                  fontSize: 14,
+                  //fontFamily: 'SF Pro Display',
+                  fontWeight: FontWeight.w400),
+            ),
+            const SizedBox(height: 3),
+            PhoneNumberInputWidget(),
+            const SizedBox(height: 12),
+            Text(
+              'Commentaries to trip',
+              style: TextStyle(
+                  color: Color(0XFF888888),
+                  fontSize: 14,
+                  //fontFamily: 'SF Pro Display',
+                  fontWeight: FontWeight.w400),
+            ),
+            const SizedBox(height: 3),
+            Container(
+              child: TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: InputBorder.none,
+                    hintStyle:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                    hintText: 'Write your wishes to trip...'),
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xff6A62B7)),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
             Consumer<MyPhoneNumber>(
               builder: (context, myPhoneNumber, child) {
                 return Text(myPhoneNumber.fullNumberPr.phoneNumber.toString());
               },
             ),
-
             CounterWidget(),
-
-                  ElevatedButton(onPressed: () {
-                    print('Phone number: {fullNumber.phoneNumber}');
-                  }, child: const Text('Submit'),),
-
-                ],
+            SizedBox(
+              width: double.maxFinite,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0XFF6A62B6),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      24,
+                    ),
+                  ),
+                  visualDensity: const VisualDensity(
+                    vertical: -4,
+                    horizontal: -4,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 14,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Submit",
+                  style: TextStyle(
+                    color: Color(0XFFFFFFFF),
+                    fontSize: 18,
+                    fontFamily: 'SF Pro Display',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ),
-            ));
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-            ),
-      body: Container(),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => show_booking_modal_sheet(context),
-      ),
-    );
+    return Placeholder();
+    // Scaffold(
+    // appBar: AppBar(
+    //       ),
+    // body: Container(),
+    //    );
   }
 }
 
@@ -109,18 +146,18 @@ class _PhoneNumberInputWidgetState extends State<PhoneNumberInputWidget> {
   PhoneNumber intNumber = PhoneNumber(isoCode: 'KG');
   //PhoneNumber fullNumber = PhoneNumber();
 
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
           decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
+            border: Border.all(color: Color(0xff6A62B7)),
             borderRadius: BorderRadius.circular(30.0), // Circular border
           ),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
             child: InternationalPhoneNumberInput(
               onInputChanged: (PhoneNumber number) {
                 //fullNumber = number;
@@ -147,14 +184,6 @@ class _PhoneNumberInputWidgetState extends State<PhoneNumberInputWidget> {
             ),
           ),
         ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            // Perform validation or other actions here
-            //print('Phone number: ${fullNumber.phoneNumber}'); // Print full phone number to console
-          },
-          child: Text('Submit'),
-        ),
       ],
     );
   }
@@ -167,21 +196,15 @@ class CounterWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final counter = Provider.of<Counter>(context);
 
-    return  Row(
-
-          children: [
-            IconButton(
-              onPressed: counter.increment,
-              icon: const Icon(Icons.add),),
-
-
-            IconButton(
-              onPressed: counter.decrement,
-              icon: const Icon(Icons.add_chart),),
-
-
-          ]
-      );
+    return Row(children: [
+      IconButton(
+        onPressed: counter.increment,
+        icon: const Icon(Icons.add),
+      ),
+      IconButton(
+        onPressed: counter.decrement,
+        icon: const Icon(Icons.add_chart),
+      ),
+    ]);
   }
 }
-
